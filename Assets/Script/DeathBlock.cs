2c62be68be1dc28f9f81b1e41b7e313b59a65afb
Rafael Mota <RIM223@live.com>
2015-11-24 13:25:52 -0500

    no message

--- Assets/Script/DeathBlock.cs
+++ Assets/Script/DeathBlock.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class DeathBlock : MonoBehaviour {
 
+	public float scrollSpeed = 0.5F;
+	public Renderer rend;
+	void Start() {
+		rend = GetComponent<Renderer>();
+	}
+	void Update() {
+		float offset = Time.time * scrollSpeed;
+		rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+	}
 	// Use this for initialization
 	void OnTriggerEnter(Collider activator)
 	{
 		Destroy(activator.gameObject);
-		Debug.Log("hello");
-
 	}
+
 }
