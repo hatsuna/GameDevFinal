768640351185a38552520fe9a456504360272ef9
Rafael Mota <RIM223@live.com>
2015-11-24 14:44:07 -0500

    no message

--- Assets/Script/DeathBlock.cs
+++ Assets/Script/DeathBlock.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class DeathBlock : MonoBehaviour {
 
-<<<<<<< HEAD
+	GameManager manager;
 	public float scrollSpeed = 0.5F;
 	public Renderer rend;
 	void Start() {
 		rend = GetComponent<Renderer>();
+		manager = FindObjectOfType<GameManager>();
 	}
 	void Update() {
 		float offset = Time.time * scrollSpeed;
@@ -16,19 +17,6 @@ public class DeathBlock : MonoBehaviour {
 	// Use this for initialization
 	void OnTriggerEnter(Collider activator)
 	{
-		Destroy(activator.gameObject);
-=======
-	GameManager manager;
-
-	void Start () {
-		manager = FindObjectOfType<GameManager>();
-	}
-
-	// Use this for initialization
-	void OnTriggerEnter(Collider activator)
-	{
-		//Destroy(activator.gameObject);
-		//Debug.Log("hello");
 		if (activator.gameObject.layer == 8){ // layer 8 is player layer
 			activator.gameObject.SetActive(false);
 		}
@@ -36,8 +24,7 @@ public class DeathBlock : MonoBehaviour {
 			manager.objectList.Remove(activator.gameObject);
 			Destroy(activator.gameObject);
 		}
-		
->>>>>>> origin/master
 	}
+
 
 }
